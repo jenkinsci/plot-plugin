d689c0c70541124f572647f91e7eadc86002a992
kohsuke <kohsuke@dev.java.net>
2007-05-14 22:53:28 +0000

    added skeleton for the plot plugin for Nigel

---
+++ src/main/java/hudson/plugins/plot/PluginImpl.java
@@ -0,0 +1,14 @@
+package hudson.plugins.plot;
+
+import hudson.Plugin;
+import hudson.tasks.BuildStep;
+
+/**
+ * Entry point of a plugin.
+ *
+ * @plugin
+ */
+public class PluginImpl extends Plugin {
+    public void start() throws Exception {
+    }
+}
